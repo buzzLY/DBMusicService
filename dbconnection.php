91ed4e17fbb9d2ed683f0737e28c60bc6ee4ecfe
Rakshit Sareen <rs5606@nyu.edu>
2017-12-14 12:32:45 -0500

    changed password

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'sareen');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'dbproject');
 // connection handler to database
 $connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Could not Connect to Database");
